d1baa20952e5dcfd13bf5243120c81b0d9f90e95
Antoine Barthelemy <antoine@linux.com>
2021-07-20 21:55:21 +0200

    feat(quick)

---
+++ fedora_quick.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+sudo dnf install -y git vim
+sudo dnf update -y
+
+# Firefox Add-ons
+wget https://addons.mozilla.org/firefox/downloads/file/3687310/adblocker_ultimate-3.7.10-an+fx.xpi -O /tmp/adblock_ultimate.xpi
+firefox /tmp/adblock_ultimate.xpi
+wget https://addons.mozilla.org/firefox/downloads/file/3803046/ghostery_privacy_ad_blocker-8.5.7-an+fx.xpi -O /tmp/ghostery.xpi
+firefox /tmp/ghostery.xpi
+
+# Gnome Theme
+wget https://github.com/EliverLara/Sweet/releases/download/2.0/Sweet-Dark.tar.xz
+tar xvf Sweet-Dark.tar.xz
+sudo mv Sweet-Dark /usr/share/themes/
+gsettings set org.gnome.desktop.interface gtk-theme "Sweet-Dark"
+gsettings set org.gnome.desktop.wm.preferences theme "Sweet-Dark"
+
+git clone https://github.com/EliverLara/candy-icons
+rm -rf candy-icons/.git candy-icons/README.md
+sudo mv candy-icons /usr/share/icons/
+gsettings set org.gnome.desktop.interface icon-theme "Candy-icons"
+
+gsettings set org.gnome.desktop.interface clock-show-weekday true
+
+# Bash
+bash -c "$(wget https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh -O -)"
+sed -i -e "s/OSH_THEME=.*/OSH_THEME=\"purity\"/g" ~/.bashrc
+
+# Vimrc
+wget https://raw.githubusercontent.com/Ant0wan/config/master/assets/vimrc -O ~/.vimrc
+
+# Rust
+wget https://sh.rustup.rs -O /tmp/rust.sh
+bash rust.sh -y
+
+echo https://doh.42l.fr/dns-query
